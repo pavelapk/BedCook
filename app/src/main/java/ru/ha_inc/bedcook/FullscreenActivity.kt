8390d654785ba02ed46b4bf28550aef3e161b914
pavelapk <pepdj@mail.ru>
2022-01-09 16:48:22 +0700

    3d model placement (cake); external dependency: arsceneview;

--- app/src/main/java/ru/ha_inc/bedcook/FullscreenActivity.kt
+++ app/src/main/java/ru/ha_inc/bedcook/FullscreenActivity.kt
@@ -1,9 +1,16 @@
 package ru.ha_inc.bedcook
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.ar.core.Anchor
+import com.google.ar.core.Config
+import io.github.sceneview.ar.arcore.depthEnabled
+import io.github.sceneview.ar.arcore.instantPlacementEnabled
+import io.github.sceneview.ar.node.ArNode
 import ru.ha_inc.bedcook.databinding.ActivityFullscreenBinding
 
 /**
@@ -13,13 +20,56 @@ import ru.ha_inc.bedcook.databinding.ActivityFullscreenBinding
 class FullscreenActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityFullscreenBinding::bind)
+    private var modelNode: ArNode? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fullscreen)
 
+        binding.sceneView.configureSession {
+            it.depthEnabled = false
+            it.instantPlacementEnabled = false
+            it.lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
+            it.cloudAnchorMode = Config.CloudAnchorMode.DISABLED
+            it.planeFindingMode = Config.PlaneFindingMode.HORIZONTAL
+            it.focusMode = Config.FocusMode.AUTO
+        }
+        binding.sceneView.onTouchAr = { hitResult, _ ->
+            Log.d("DADAYA", hitResult.distance.toString())
+            Log.d("DADAYA", hitResult.hitPose.toString())
+            anchorOrMove(hitResult.createAnchor())
+        }
+
+        binding.dummyButton.setOnClickListener {
+            modelNode?.apply {
+                binding.sceneView.removeChild(this)
+                destroy()
+            }
+            modelNode = null
+        }
     }
 
+    private fun anchorOrMove(anchor: Anchor) {
+        if (modelNode == null) {
+//            isLoading = true
+            modelNode = ArNode(
+                context = this,
+                coroutineScope = lifecycleScope,
+                anchor = anchor,
+                modelGlbFileLocation = "cake.glb",
+                onModelLoaded = {
+//                    actionButton.text = getString(R.string.move_object)
+//                    actionButton.icon = resources.getDrawable(R.drawable.ic_target)
+//                    isLoading = false
+                }).also {
+                it.positionY = 0f
+                it.scale = 1f
+                binding.sceneView.addChild(it)
+            }
+        } else {
+            modelNode?.anchor = anchor
+        }
+    }
 
 }
